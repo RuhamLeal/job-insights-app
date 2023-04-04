e85aed8622af359cdf572baa5241adb36bd31063
Ruham Leal <ruhamxlpro@hotmail.com>
2023-04-04 11:14:20 -0300

    test: Adiciona teste para contar ocorrencias de todos os trabalhos no arquivo CSV

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    counter = count_ocurrences("data/jobs.csv", "Developer")
+    assert counter == 352
